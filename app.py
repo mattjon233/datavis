96d6587d6500faff9ee628f1b6fefa5dd909a3c3
Matthews Jones <matthews.vasconcelos@funceme.br>
2022-02-01 11:19:15 -0300

    app fix

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask_caching import Cache
 
 # Command to create Dash app
 app = dash.Dash(__name__, prevent_initial_callbacks=True)
+server = app.server
+
 app.title = 'Visualização de Dados'
 app.config.suppress_callback_exceptions = True
 app.config.update({
@@ -10,7 +12,6 @@ app.config.update({
 'include_asset_files': True,
 'assets_external_path': '',
 })
-server = app.server
 cache = Cache(server, config=
    {
    'CACHE_TYPE': 'filesystem',
