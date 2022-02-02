90d93a87149a60d3eb2380f0b6eecc71c5eb5106
Matthews Jones <matthews.vasconcelos@funceme.br>
2022-02-02 09:05:53 -0300

    .

--- page.py
+++ page.py
@@ -13,7 +13,10 @@ body = html.Div(
         #         dbc.Col(id='div-main',
         #             children=[
                         html.Iframe(src='https://observablehq.com/embed/@mattjon233/projeto-final-datavis?cells=viewof+dashboard',
-                                    style={"height": "435px", "width": "100%"})
+                                    style={"height": "435px", "width": "100%"}),
+
+                        html.Iframe(src='https://observablehq.com/embed/@mattjon233/projeto-final-datavis?cells=spiMapGeoJson',
+                                    style={"height": "700px", "width": "100%"})
         #             ]
         #         )
         #     ]
